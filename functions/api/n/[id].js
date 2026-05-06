3b86b482eeaafa4dec030a1b418f4d576b00f0a5
Nathan Puls <71989998+nathanpuls@users.noreply.github.com>
2026-05-06 14:03:07 -0500

    Delete functions directory

--- functions/api/n/[id].js
+++
@@ -1,1 +0,0 @@
-export { onRequestGet } from "../../n/[id].js";
--- functions/api/save.js
+++
@@ -1,9 +0,0 @@
-export async function onRequestPost({ request, env }) {
-  const { content } = await request.json();
-
-  const id = crypto.randomUUID().slice(0, 8);
-
-  await env.ENDS_NOTES.put(id, content);
-
-  return Response.json({ id });
-}
